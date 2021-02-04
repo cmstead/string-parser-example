e070d584e75a017c1f2dde3d6878d20e9836bca7
Chris Stead <cmstead@gmail.com>
2021-02-04 14:29:36 -0800

    [!Refactor] Simplified logic around character comparison

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 function parseString(stringToParse) {
 
     let characterArray = [];
+    // let currentCharacterTuple = null;
+    let lastCharacter = null;
 
     stringToParse.split('').forEach(function (character) {
         const lastCharacterTuple = characterArray[characterArray.length - 1];
-        const lastCharacter = characterArray.length > 0
-            ? lastCharacterTuple[0]
-            : null;
 
         if (lastCharacter !== character) {
             characterArray.push([character, 1]);
         } else {
             lastCharacterTuple[1]++;
         }
+
+        lastCharacter = character;
     });
 
     return characterArray;
