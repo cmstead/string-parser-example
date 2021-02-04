5f2d1950f80eb12953c2e4456ad9911fb3ac91aa
Chris Stead <cmstead@gmail.com>
2021-02-04 14:27:09 -0800

    [Feature] Added support for multiple characters in different positions

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ function parseString(stringToParse) {
     } else {
         let characterArray = [];
 
-        stringToParse.split('').forEach(function(character) {
-            characterArray.push([character, 1]);
+        stringToParse.split('').forEach(function (character) {
+            const lastCharacterTuple = characterArray[characterArray.length - 1];
+            const lastCharacter = characterArray.length > 0
+                ? lastCharacterTuple[0]
+                : null;
+
+            if (lastCharacter !== character) {
+                characterArray.push([character, 1]);
+            } else {
+                lastCharacterTuple[1]++;
+            }
         });
 
         return characterArray;
