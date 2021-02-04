de85ae6f9e84dfd4c8b2ebaf56268cda84245075
Chris Stead <cmstead@gmail.com>
2021-02-04 14:23:45 -0800

    [Refactor] Removed unnecessary condition

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 function parseString(stringToParse) {
-    if(stringToParse.length === 1) {
-        return [['a', 1]];
-    } else if (stringToParse === 'aa') {
+    if (stringToParse === 'aa') {
         return [['a', stringToParse.length]];
     } else {
         let characterArray = [];
